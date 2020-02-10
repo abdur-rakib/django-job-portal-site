0ff37403c9eb08bab2e48b188280777167772645
Abdur Rakib <43021476+RakibWebDeveloper@users.noreply.github.com>
2020-02-10 21:58:22 +0600

    Update settings.py

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -135,4 +135,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = "accounts.user"
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
