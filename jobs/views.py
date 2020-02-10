4ba5e18708758c2bd138753ca7602ad44b656ebd
RakibWebDeveloper <abdurrakib961@gmail.com>
2020-02-10 18:06:13 +0600

    some other things

--- jobs/views.py
+++ jobs/views.py
@@ -202,9 +202,13 @@ class AboutView(CreateView):
     model = About
     template_name = 'about.html'
     form_class = AboutForm
-    extra_context = {
-        'about_page': 'active'
-    }
+
+    def get_context_data(self, **kwargs):
+        
+        return super().get_context_data(**kwargs)
+        context['about_page'] = 'active'
+        return context
+
 
     def get(self, request, *args, **kwargs):
         context = {'form': AboutForm()}
